69cf63198feda2a729097100c03c5a5888d64fd5
NasWin11 <1270071138@qq.com>
2025-08-17 11:09:35 +0800

    init

--- mcts/MCTS_Agent.py
+++ mcts/MCTS_Agent.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from board import GomokuBoard
 from mcts.MCTS_Node import MCTS_Node
 from net.GomokuNet import PolicyValueNet
 
@@ -12,4 +13,7 @@ class MCTSAgent:
         self.device = device
         self.model.to(self.device)
         self.model.eval()
-        self.visit_nodes:List['MCTS_Node'] = []
+        self.visit_nodes: List['MCTS_Node'] = []
+
+    def run(self, root_board: GomokuBoard, player: int, number_samples=100, is_train=False):
+        root_node = MCTS_Node(root_board, player)
