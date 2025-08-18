96a8e7217e9b6400cec66115266553c0a7cd1504
NasNeo <1270071138@qq.com>
2025-08-18 09:27:04 +0800

    init

--- board/PyGameBoard.py
+++ board/PyGameBoard.py
@@ -168,9 +168,9 @@ class PygameMatch:
 if __name__ == "__main__":
     # Example usage: human vs random agent
     model = PolicyValueNet()
-    path = r"C:\Users\12700\Downloads\strong_model_30.pth"
+    path = r"../check_dir/run3/model/strong_model_90.pth"
     if os.path.exists(path):
-        model.load_state_dict(torch.load(path))
+        model.load_state_dict(torch.load(path,map_location="cpu"))
     modelAgent = MCTSAgent(model)
     # game = PygameMatch(modelAgent, modelAgent)
     game = PygameMatch(modelAgent, modelAgent)
