9a9bce33540e710f81e3b0efbc6eba833a65586e
NasNeo <1270071138@qq.com>
2025-08-20 17:20:19 +0800

    init

--- Test/TestGenBorad.py
+++ Test/TestGenBorad.py
@@ -6,7 +6,7 @@ from train import generate_random_safe_board
 # 测试
 # -------------------------------
 if __name__ == "__main__":
-    b = generate_random_safe_board(board_size=19)
+    b = generate_random_safe_board(board_size=15)
     print("随机非终局棋盘生成完成")
     print("棋盘步数:", b.move_count)
     print("是否终局:", b.is_terminal())
